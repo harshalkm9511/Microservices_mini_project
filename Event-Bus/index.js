878d7d21865d843b8ce0aedbc249d90b52638e51
Harshal Mahajan <hm2672746@gmail.com>
2026-09-13 11:29:05 +0530

    created the Event-Bus from scratch as the another service that receive an event in the form of incomming http request and emmits an event to all the services

---
+++ Event-Bus/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/events", async (req, res) => {
+
+    await axios.post(`http://localhost:1000/events`, { event: req.body });
+    await axios.post(`http://localhost:1001/events`, { event: req.body });
+    await axios.post(`http://localhost:1000/events`, { event: req.body });
+
+    res.send({ status: "ok" });
+});
+
+app.listen(4000, () => {
+    console.log("Event-Bus server is running on port 4000");
+});
--- posts/index.js
+++ posts/index.js
@@ -1,28 +1,41 @@
 const express = require("express");
-const {randomBytes} = require("crypto");
+const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 let posts = {};
-app.get("/posts", (req, res)=>{
+app.get("/posts", (req, res) => {
     res.send(posts);
 });
 
-app.post("/posts", (req, res)=>{
-    let {title} = req.body;
+app.post("/posts", async (req, res) => {
+    let { title } = req.body;
     let id = randomBytes(16).toString("hex");
+
     posts[id] = {
         id,
         title
     };
+
+    await axios.post("http://localhost:4000/events", {
+        type: "created_Post",
+        data: { id, title }
+    });
+
     res.status(200).send(posts[id]);
 });
 
-app.listen(1000, ()=>{
+app.post("/events", (req, res) => {
+    console.log("event created  "+req.body.event.type);
+    res.send({});
+});
+
+app.listen(1000, () => {
     console.log("server is running on port 1000");
 });
